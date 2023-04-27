08f5bce488e74ec1cad09b37d80e6cdac3d735b4
Steven Zhu <65844236+steveyxz@users.noreply.github.com>
2023-04-27 21:20:33 +1000

    Added more adapter types

--- src/main/java/me/partlysunny/configurate/impl/Adapters.java
+++ src/main/java/me/partlysunny/configurate/impl/Adapters.java
@@ -1,16 +1,25 @@
 package me.partlysunny.configurate.impl;
 
 import me.partlysunny.configurate.api.DataAdapter;
+import net.kyori.adventure.key.Key;
+import net.kyori.adventure.sound.Sound;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.Color;
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.MemoryConfiguration;
 import org.bukkit.configuration.MemorySection;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 import java.util.Objects;
+import java.util.UUID;
 
 public class Adapters {
 
@@ -62,6 +71,93 @@ public class Adapters {
             section.set("particles", object.hasParticles());
             section.set("icon", object.hasIcon());
             return section;
+        }
+    };
+
+    public static final DataAdapter<Color, ConfigurationSection> COLOR = new DataAdapter<>() {
+        @Override
+        public ConfigurationSection serialize(Color object) {
+            ConfigurationSection section = new MemoryConfiguration();
+            section.set("red", object.getRed());
+            section.set("green", object.getGreen());
+            section.set("blue", object.getBlue());
+            return section;
+        }
+
+        @Override
+        public Color deserialize(ConfigurationSection object) {
+            return Color.fromRGB(object.getInt("red"), object.getInt("green"), object.getInt("blue"));
+        }
+    };
+
+    public static final DataAdapter<Component, String> COMPONENT = new DataAdapter<>() {
+        @Override
+        public String serialize(Component object) {
+            return MiniMessage.miniMessage().serialize(object);
+        }
+
+        @Override
+        public Component deserialize(String object) {
+            return MiniMessage.miniMessage().deserialize(object);
+        }
+    };
+
+    public static final DataAdapter<Sound, ConfigurationSection> SOUND = new DataAdapter<>() {
+        @Override
+        public ConfigurationSection serialize(Sound object) {
+            ConfigurationSection section = new MemoryConfiguration();
+            section.set("name", object.name());
+            section.set("source", object.source().name());
+            section.set("volume", object.volume());
+            section.set("pitch", object.pitch());
+            return section;
+        }
+
+        @Override
+        public Sound deserialize(ConfigurationSection object) {
+            return Sound.sound(
+                    Key.key(object.getString("name")),
+                    Sound.Source.valueOf(object.getString("source")),
+                    (float) object.getDouble("volume"),
+                    (float) object.getDouble("pitch")
+            );
+        }
+    };
+
+    public static final DataAdapter<Material, String> MATERIAL = new DataAdapter<>() {
+        @Override
+        public String serialize(Material object) {
+            return object.name();
+        }
+
+        @Override
+        public Material deserialize(String object) {
+            return Material.valueOf(object);
+        }
+    };
+
+    public static final DataAdapter<AttributeModifier, ConfigurationSection> ATTRIBUTE_MODIFIER = new DataAdapter<>() {
+        @Override
+        public ConfigurationSection serialize(AttributeModifier object) {
+            ConfigurationSection section = new MemoryConfiguration();
+            section.set("name", object.getName());
+            section.set("amount", object.getAmount());
+            section.set("operation", object.getOperation().name());
+            EquipmentSlot slot = object.getSlot();
+            if (slot != null) section.set("slot", slot.name());
+            return section;
+        }
+
+        @Override
+        public AttributeModifier deserialize(ConfigurationSection object) {
+            String name = object.getString("name");
+            return new AttributeModifier(
+                    UUID.fromString(name),
+                    name,
+                    object.getDouble("amount"),
+                    AttributeModifier.Operation.valueOf(object.getString("operation")),
+                    EquipmentSlot.valueOf(object.getString("slot"))
+            );
         }
     };
 
